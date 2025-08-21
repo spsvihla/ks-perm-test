26d85b16df8e4677d3b6cafe5607cc1fa07e06b3
Sean Svihla <spsvihla@gmail.com>
2025-08-21 09:51:11 -0600

    Fix: type error

--- src/kspt.cpp
+++ src/kspt.cpp
@@ -149,7 +149,7 @@ make_prefix_cumhist(double* data, int size,
     std::vector<int> hist(bins.num_bins, 0);
     int next = 0;
 
-    int recip = bins.bin_width;
+    double recip = bins.bin_width;
 
     // this loop might be hard to optimize -- so unroll it manually
     for (int i = 0; i + 3 < size; i += 4) {
